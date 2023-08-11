0ca8b7db8d6b28b664678c66d9573fb029f333a0
John-Flavian <flavianomeokwe@gmail.com>
2023-08-11 07:44:07 +0100

    feat: Adds the footer section of the navbar.

--- src/app/components/NavBar.jsx
+++ src/app/components/NavBar.jsx
@@ -51,7 +51,7 @@ const navLinks = [
 
 const NavBar = () => {
   return (
-    <nav className="px-[20px] w-[300px] border-solid border-2">
+    <nav className="px-[20px] w-[300px] mb-4 border-solid border-2 overflow-y-auto">
       {/* Header Section */}
       <header className="flex mt-5 mb-5">
         <Image src={logo} alt="logo" width={32} height={32} />
@@ -68,12 +68,12 @@ const NavBar = () => {
       </div>
       {/* NavLinks Section  */}
       <div className="mt-4">
-        <ul className="list-none md:flex flex-col gap-10">
+        <ul className="list-none md:flex flex-col gap-1">
           {navLinks.map((link, index) => {
             return (
               <li
                 key={index}
-                className="relative flex items-center my-1 py-2 hover:bg-gray-100 hover:font-bold rounded-lg"
+                className="relative flex items-center py-2 hover:bg-gray-100 hover:font-bold rounded-lg"
               >
                 {link.icon}
                 <p className="text-gray-700">{link.title}</p>
@@ -85,18 +85,18 @@ const NavBar = () => {
       </div>
       {/* Support and features section */}
       <div className="mt-5">
-        <ul className="list-none md:flex flex-col gap-10">
-          <li className="flex items-center my-2 hover:bg-gray-100 hover:font-bold py-2 rounded-lg">
+        <ul className="list-none md:flex flex-col gap-1">
+          <li className="flex items-center hover:bg-gray-100 hover:font-bold py-2 rounded-lg">
             <HiOutlineSupport className={iconStyle} />
             <p className="text-gray-700">Support</p>
           </li>
-          <li className="flex items-center my-2 hover:bg-gray-100 hover:font-bold py-2 rounded-lg">
+          <li className="flex items-center hover:bg-gray-100 hover:font-bold py-2 rounded-lg">
             <FiSettings className={iconStyle} />
             <p className="text-gray-700">Settings</p>
           </li>
         </ul>
-
-        <div className="mt-10 py-6 bg-gray-100 rounded-lg flex-col items-center justify-center px-4">
+        {/* Features */}
+        <div className="mt-5 py-6 bg-gray-100 rounded-lg flex-col items-center justify-center px-4">
           <div className="my-1">
             <h4 className="font-bold">New features available!</h4>
             <p className="text-gray-700 my-2 text-[15px]">
@@ -104,22 +104,43 @@ const NavBar = () => {
             </p>
           </div>
           <div>
-            <div className="my-8 rounded-lg">
+            <div className="my-6 rounded-lg">
               <iframe
                 className="rounded-lg"
                 width="220"
                 height="200"
                 src="https://www.youtube.com/embed/k4szRq1R-08"
                 title="YouTube video player"
-                frameborder="0"
                 allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-                allowfullscreen
+                allowFullScreen
               ></iframe>
             </div>
             <div className="flex-start mt-4 space-x-5">
               <button className="text-gray-700">Dismiss</button>
               <button className="text-purple-500 font-bold">What's new?</button>
             </div>
+          </div>
+        </div>
+        <div className="w-full border-t my-8" />
+        {/* Account */}
+        <div className="mb-4 mx-auto flex items-center w-full space-x-5">
+          <div className="flex-auto">
+            <Image
+              className="rounded-full"
+              height={50}
+              width={50}
+              src={"/images/woman-1.jpg"}
+              alt="profile-picture"
+            />
+          </div>
+          <div className="flex-auto">
+            <h4 className="font-bold">Olivia Rhye</h4>
+            <span className="text-[14px] text-gray-700">
+              olivia@untiltledui.com
+            </span>
+          </div>
+          <div className="flex-auto">
+            <GoSignOut className={iconStyle} />
           </div>
         </div>
       </div>
