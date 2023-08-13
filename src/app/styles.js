1e3383e4ebb5b96520cb0ff9c58fb3c488f1dfe1
John-Flavian <flavianomeokwe@gmail.com>
2023-08-13 18:16:58 +0100

    feat: adds the transition effect on the navlinks and the dashboard links.

--- src/app/styles.js
+++ src/app/styles.js
@@ -1,12 +1,12 @@
 const iconStyle = "mr-2 text-gray-500 text-[30px]";
 const navLiStyle =
-  "relative flex items-center hover:bg-gray-100 hover:font-bold py-2 rounded-lg";
+  "relative flex items-center transition-bg duration-300 hover:bg-gray-100 hover:font-bold py-2 rounded-lg";
 const badgeStyle =
   "absolute ml-2 bg-gray-300 text-black text-xs px-3 py-1 rounded-full top-3.5 right-5 transform translate-x-1/2 -translate-y-1/2";
 
 const dashboardNavUlStyle = "list-none flex border border-gray-300 rounded-lg";
 const dashboardNavLiStyle =
-  "font-medium hover:bg-gray-100 hover:font-semibold text-gray-700 cursor-pointer border-gray-200 p-3 text-center";
+  "font-medium transition-bg duration-300 hover:bg-gray-100 hover:font-semibold text-gray-700 cursor-pointer border-gray-200 p-3 text-center";
 const paymentDivStyle =
   "relative flex items-start py-5 px-2 w-full border-solid border-2 shadow-md rounded-lg";
 
